e902954b38a1ead82c66865640bd0cdc3955b57f
scheovla, john <schevola@sbcglobal.net>
2021-03-18 00:28:36 -0400

    another basic test

---
+++ app/main.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['GET'])
+def test():
+    return jsonify({
+        'success': True,
+        'action': 'HelloWorld'
+    })
