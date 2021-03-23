e1b4548a226d4f2e74b42b7111a4aa89581f0ada
scheovla, john <schevola@sbcglobal.net>
2021-03-22 20:49:20 -0400

    another update to get unit tests to work and application to run in hek

--- app/main.py
+++ app/main.py
@@ -4,64 +4,63 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from app.auth.auth import authRequired, AuthnError
 
-def create_app(test_config=None):
-    app = Flask(__name__)
-    CORS(app)
+
+app = Flask(__name__)
+CORS(app)
 
 
-    @app.route('/', methods=['GET'])
-    def healthCheck():
-        return jsonify({
-            'success': True,
-            'action': 'Healthy'
-        })
+@app.route('/', methods=['GET'])
+def healthCheck():
+    return jsonify({
+        'success': True,
+        'action': 'Healthy'
+    })
 
 
-    @authRequired(permission='get:actors')
-    @app.route('/actors', methods=['GET'])
-    def getActors(payload):
-        pass
+@authRequired(permission='get:actors')
+@app.route('/actors', methods=['GET'])
+def getActors(payload):
+    pass
 
 
-    @authRequired(permission='post:actors')
-    @app.route('/actors', methods=['POST'])
-    def addActors(payload):
-        pass
+@authRequired(permission='post:actors')
+@app.route('/actors', methods=['POST'])
+def addActors(payload):
+    pass
 
 
-    @authRequired(permission='patch:actors')
-    @app.route('/actors', methods=['PATCH'])
-    def updateActors(payload):
-        pass
+@authRequired(permission='patch:actors')
+@app.route('/actors', methods=['PATCH'])
+def updateActors(payload):
+    pass
 
 
-    @authRequired(permission='delete:actors')
-    @app.route('/actors', methods=['DELETE'])
-    def deleteActors(payload):
-        pass
+@authRequired(permission='delete:actors')
+@app.route('/actors', methods=['DELETE'])
+def deleteActors(payload):
+    pass
 
 
-    @authRequired(permission='get:movies')
-    @app.route('/movies', methods=['GET'])
-    def getMovies(payload):
-        pass
+@authRequired(permission='get:movies')
+@app.route('/movies', methods=['GET'])
+def getMovies(payload):
+    pass
 
 
-    @authRequired(permission='post:movies')
-    @app.route('/movies', methods=['POST'])
-    def addMovies(payload):
-        pass
+@authRequired(permission='post:movies')
+@app.route('/movies', methods=['POST'])
+def addMovies(payload):
+    pass
 
 
-    @authRequired(permission='patch:movies')
-    @app.route('/movies', methods=['PATCH'])
-    def updateMovies(payload):
-        pass
+@authRequired(permission='patch:movies')
+@app.route('/movies', methods=['PATCH'])
+def updateMovies(payload):
+    pass
 
 
-    @authRequired(permission='delete:movies')
-    @app.route('/movies', methods=['DELETE'])
-    def deleteMovies(payload):
-        pass
+@authRequired(permission='delete:movies')
+@app.route('/movies', methods=['DELETE'])
+def deleteMovies(payload):
+    pass
 
-    return app
--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 import unittest
-from app.main import create_app
+from app.main import app
 
 
 class CastingAgencyTestCase(unittest.TestCase):
@@ -7,15 +7,16 @@ class CastingAgencyTestCase(unittest.TestCase):
 
     def setUp(self):
         """Define test variables and initialize app."""
+        self.app = app
+        self.client = self.app.test_client()
         self.questionText = "asdfooiuqwerlkjsfamoipiuqwlekrjasdfpoiqjweasdjkhfauihebagsjk"
-        self.app = create_app()
-        self.client = self.app.test_client
 
     def test_healthCheck_success(self):
-        result = self.client().get('/')
-        self.assertEqual(200, result.status_code)
-        self.assertTrue(result.json['success'])
-        self.assertEqual("Healthy", result.json['action'])
+            result = self.client.get('/')
+            self.assertEqual(200, result.status_code)
+            self.assertTrue(result.json['success'])
+            self.assertEqual("Healthy", result.json['action'])
+
 
 
 if __name__ == "__main__":
